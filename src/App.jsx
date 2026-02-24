132be836e8ff27819cd79f8c328c472d4f2345d2
leeyongjin <dragon02229@naver.com>
2026-02-24 22:24:01 +0900

    Revert "Revert "UIUX 수정""
    This reverts commit 0394a89f4ce4f626702fc5e4177655bddf6bf4e4.

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,11 @@ export default function App() {
       </header>
       <main className="ds-app-main" id="main-content" role="main">
         <TabPanel id="panel-main" value={TAB_MAIN} activeValue={view}>
-          <MainScreen botRunning={botRunning} botError={botError} />
+          <MainScreen
+          botRunning={botRunning}
+          botError={botError}
+          onOpenSettings={() => setView(TAB_SETTINGS)}
+        />
         </TabPanel>
         <TabPanel id="panel-settings" value={TAB_SETTINGS} activeValue={view}>
           <Settings />
